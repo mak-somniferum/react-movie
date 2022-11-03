458830b28cc7b48d2f5abbe25cc1588b57690ea1
mak-somniferum <suhjin700@gmail.com>
2022-11-03 16:13:33 +0900

    .

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,7 +9,7 @@ function Movie({id, coverImg, title, summary, genres}) {
       <ul>
         {genres.map(g => <li key={g}>{g}</li>)}
       </ul>
-      <p>{summary}</p>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
     </div>  
   );
 }
